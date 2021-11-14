399713283fc9ba4510287ee303a63702f73cc048
Raymond Schweers <raymond@hetwebbureau.nl>
2021-11-14 16:35:42 +0100

    init

---
+++ macos.sh
@@ -0,0 +1,10 @@
+#  Apple's command-line tools
+xcode-select --install
+
+# Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew update && brew upgrade
+brew install python ansible
+
+# use installed python (not default from OS)
+export PATH="/usr/local/opt/python/libexec/bin:$PATH"
