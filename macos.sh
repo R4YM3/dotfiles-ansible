5d63082d9901fef53d14b6f573609fc2048a324b
Raymond Schweers <raymond@hetwebbureau.nl>
2021-11-24 00:43:46 +0100

    feat(homebrew) check if already installed

--- macos.sh
+++ macos.sh
@@ -2,9 +2,12 @@
 xcode-select --install
 
 # Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew update && brew upgrade
-brew install python ansible
+which -s brew
+if [[ $? != 0 ]] ; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    brew update && brew upgrade
+    brew install python ansible
+fi
 
 # use installed python (not default from OS)
 export PATH="/usr/local/opt/python/libexec/bin:$PATH"
