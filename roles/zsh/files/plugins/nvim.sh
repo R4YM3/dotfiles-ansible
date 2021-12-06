ce552623ca37512b24dbe4b1e4166a26a0125fe0
Raymond Schweers <raymond@hetwebbureau.nl>
2021-12-06 23:12:40 +0100

    fix(nvm) install in ~/.nvim

--- roles/zsh/files/plugins/nvim.sh
+++ roles/zsh/files/plugins/nvim.sh
@@ -1,3 +1,3 @@
-export NVM_DIR="{{ ansible_env.USER }}/.nvm"
+export NVM_DIR="~/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
