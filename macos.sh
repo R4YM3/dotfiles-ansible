0d585a2a187c6085439564cc6b5307a681ecc94c
Raymond Schweers <raymond@hetwebbureau.nl>
2021-12-06 23:18:23 +0100

    feat(installer) intall ansible if not installed

--- macos.sh
+++ macos.sh
@@ -1,12 +1,16 @@
 #  Apple's command-line tools
 xcode-select --install
 
-# Homebrew
 which -s brew
 if [[ $? != 0 ]] ; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     brew update && brew upgrade
     brew install python ansible
+fi
+
+which -s ansible
+if [[ $? != 0 ]] ; then
+    brew install ansible
 fi
 
 # use installed python (not default from OS)
