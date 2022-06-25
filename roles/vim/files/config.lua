8072d28981a9b55d6ee0f66252d2dfd4961e97f5
Raymond Schweers <raymond@hetwebbureau.nl>
2022-06-25 13:08:59 +0200

    feat(vim) indent-blankline

--- roles/vim/files/config.lua
+++ roles/vim/files/config.lua
@@ -166,6 +166,15 @@ lvim.plugins = {
     "p00f/nvim-ts-rainbow",
   },
   {
+    "lukas-reineke/indent-blankline.nvim",
+    config = require("indent_blankline").setup {
+      -- for example, context is off by default, use this to turn it on
+      show_current_context = true,
+      show_current_context_start = true,
+      show_end_of_line = true,
+    }
+  },
+  {
     "romgrk/nvim-treesitter-context",
     config = function()
       require("treesitter-context").setup {
@@ -186,7 +195,6 @@ lvim.plugins = {
       }
     end
   },
-
   {
     "nvim-telescope/telescope-project.nvim",
     event = "BufWinEnter",
