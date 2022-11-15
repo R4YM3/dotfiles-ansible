f094a9a1390a1c5d27b772d6e2da116aea1dc2a8
Raymond Schweers <raymond@hetwebbureau.nl>
2022-11-15 22:55:29 +0100

    feat(macos) conditionally install command line tools and rosetta

--- macos.sh
+++ macos.sh
@@ -1,20 +1,34 @@
+echo "";
+
 #  Apple's command-line tools
-xcode-select --install
+if type xcode-select >&- && xpath=$( xcode-select --print-path ) &&
+   test -d "${xpath}" && test -x "${xpath}" ; then
+   echo 'Apple Command-line tools already installed'
+else
+   xcode-select --install
+   # Enable command line tools
+   sudo xcode-select --switch /Library/Developer/CommandLineTools
+fi
+
 
 # Install rosetta
-/usr/sbin/softwareupdate --install-rosetta --agree-to-license
+/usr/bin/pgrep -q oahd && echo 'Rosetta already installed' || /usr/sbin/softwareupdate --install-rosetta --agree-to-license
 
+# install homebrew
 which -s brew
 if [[ $? != 0 ]] ; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     
     echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
     eval "$(/opt/homebrew/bin/brew shellenv)"
-    
+
     brew update && brew upgrade
+
+    # install required dependecies
     brew install python ansible
 fi
 
+# install ansible
 which -s ansible
 if [[ $? != 0 ]] ; then
     brew install ansible
